439c0ffb79975c08d93d455f8471a1548172b1c9
Chris Tang <cwolf0913@gmail.com>
2017-04-28 14:07:26 -0400

    Fixed CookieService nullPointerException

--- src/main/java/com/forestnewark/service/CookieService.java
+++ src/main/java/com/forestnewark/service/CookieService.java
@@ -13,42 +13,46 @@ import javax.servlet.http.HttpServletRequest;
 public class CookieService {
 
 
-    public void readLoginCookies(ModelMap model,HttpServletRequest request){
+    public void readLoginCookies(ModelMap model,HttpServletRequest request) {
 
         //Check for and read cookie values for saved user login credentials
         Cookie[] cookies = request.getCookies();
-        if (cookies.length > 0) {
-            //Loop Through All Cookies
-            for (int i = 0; i < cookies.length; i++) {
 
-                //Check for UserName Cookie
-                if (cookies[i].getName().equals("username")) {
-                    //Set UserName to Model Attribute
-                    model.put("currentUser", cookies[i].getValue());
-                }
 
-                //Check for rememberMe Cookie
-                if (cookies[i].getName().equals("rememberMe")) {
-                    //Check for Value of Remember Me
-                    if (cookies[i].getValue().equals("true")) {
-                        //If rememberMe - Turn rememberMe Value On
-                        model.addAttribute("rememberMe", "on");
-                    } else {
+        if (cookies != null) {
 
-                        //If not rememeberMe - Turn rememberMe Value Off
-                        model.addAttribute("rememberMe", "off");
+
+            if (cookies.length > 0) {
+                //Loop Through All Cookies
+                for (int i = 0; i < cookies.length; i++) {
+
+                    //Check for UserName Cookie
+                    if (cookies[i].getName().equals("username")) {
+                        //Set UserName to Model Attribute
+                        model.put("currentUser", cookies[i].getValue());
                     }
-                }
 
+                    //Check for rememberMe Cookie
+                    if (cookies[i].getName().equals("rememberMe")) {
+                        //Check for Value of Remember Me
+                        if (cookies[i].getValue().equals("true")) {
+                            //If rememberMe - Turn rememberMe Value On
+                            model.addAttribute("rememberMe", "on");
+                        } else {
+
+                            //If not rememeberMe - Turn rememberMe Value Off
+                            model.addAttribute("rememberMe", "off");
+                        }
+                    }
+
+                }
             }
+
+
         }
 
 
-
-
     }
-
-
 
 
 
