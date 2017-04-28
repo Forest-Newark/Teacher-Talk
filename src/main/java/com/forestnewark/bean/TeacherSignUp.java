839021e270d393ba4eacf51deffa41463bce5f02
Chris Tang <cwolf0913@gmail.com>
2017-04-28 11:36:03 -0400

    added Teacher Registration, Controller and HTML

---
+++ src/main/java/com/forestnewark/bean/TeacherSignUp.java
@@ -0,0 +1,90 @@
+package com.forestnewark.bean;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "teachertalk")
+public class TeacherSignUp {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private Integer phoneNumber;
+    private String subjectArea;
+
+    public TeacherSignUp() {
+    }
+
+    public TeacherSignUp(String firstName, String lastName, String email, String password, Integer phoneNumber, String messageSubject) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.subjectArea = messageSubject;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Integer phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getSubjectArea() {
+        return subjectArea;
+    }
+
+    public void setSubjectArea(String subjectArea) {
+        this.subjectArea = subjectArea;
+    }
+}
