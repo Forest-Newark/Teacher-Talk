c24c01ef79f9a1cbef52a8eaab3c911b0d11348f
RoganRhysShane <chrisoliver08@live.com>
2017-05-02 09:29:41 -0400

    log class complete

--- src/main/java/com/forestnewark/bean/Log.java
+++ src/main/java/com/forestnewark/bean/Log.java
@@ -9,10 +9,14 @@ import javax.persistence.Id;
  * Form on top of log that lets you search. logservice.save. make another repointerface for log table. in database service need to autowire in logrepo.
  * Created by cmitchell on 5/1/17.
  */
+//creating log class for the message log for the teacher to view previous/recent messages that the teacher sent to the parents/guardians regarding the student/child
 
+//this tells JPA that the log class is a JPA entity and can be persisted to a database
 @Entity
 public class Log {
-
+//using @Id and @GeneratedValue for hibernate.
+// @Id this tells JPA that private int id is the primary key.
+// @Generated Value this tells JPA that is should automatically generate this key for this entity
     @Id
     @GeneratedValue
     private int id;
@@ -25,7 +29,7 @@ public class Log {
 
     public Log() {
     }
-
+//setting Contructors
     public Log(String messageSentTo, String studentName, String parentName, String templatesent, String notes) {
         this.messageSentTo = messageSentTo;
         this.studentName = studentName;
@@ -33,7 +37,7 @@ public class Log {
         this.templatesent = templatesent;
         this.notes = notes;
     }
-
+//setting getters and setters
     public int getId() {
         return id;
     }
