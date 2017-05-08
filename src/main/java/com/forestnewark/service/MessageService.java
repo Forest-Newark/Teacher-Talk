72d64127e28862850ebb0da3454f90064b45a284
RoganRhysShane <chrisoliver08@live.com>
2017-05-08 10:30:19 -0400

    back button added to message log

--- src/main/java/com/forestnewark/service/MessageService.java
+++ src/main/java/com/forestnewark/service/MessageService.java
@@ -19,14 +19,18 @@ public class MessageService {
 
     private final DatabaseService ds;
 
+    final
+    TranslationService ts;
+
 
     private final String username = "teacher.talk.tiy@gmail.com";
     private final String password = "tiyteachertalk";
 
 
     @Autowired
-    public MessageService(DatabaseService ds) {
+    public MessageService(DatabaseService ds, TranslationService ts) {
         this.ds = ds;
+        this.ts = ts;
     }
 
 
@@ -35,7 +39,7 @@ public class MessageService {
      * @param studentId of the student who will recieve the message
      * @param messageName for the template of the message to be sent
      */
-    public void sendMessage(String studentId, String messageName, String currentUserEmail) {
+    public void sendMessage(String studentId, String messageName, String currentUserEmail,String messageText) {
         System.out.println("I made it send message ms");
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
@@ -57,7 +61,7 @@ public class MessageService {
             message.setRecipients(Message.RecipientType.TO,
                     InternetAddress.parse(ds.getStudentById(studentId).getParent().getPrimaryEmail())); //Receiver's Email Address (TO)
             message.setSubject("Teacher Talk Test: Student Name: " + ds.getStudentById(studentId).getStudentFirstName()); // Subject Line
-            message.setText(this.messageBuilder(studentId,messageName,currentUserEmail)); // Body Text
+            message.setText(this.messageBuilder(studentId,messageName,currentUserEmail,messageText)); // Body Text
 
             Transport.send(message);
 
@@ -69,17 +73,19 @@ public class MessageService {
 
     }
 
-    public String messageBuilder(String studentId, String messageName,String currentUserEmail){
+    public String messageBuilder(String studentId, String messageName,String currentUserEmail, String messageText){
 
         StringBuilder sb = new StringBuilder();
         sb.append("To the Parent(s) or Guardian(s) of: ");
         sb.append(ds.getStudentById(studentId).getStudentFirstName() +" " +ds.getStudentById(studentId).getStudentLastName());
         sb.append("\n\n");
-        if(ds.getStudentById(studentId).getParent().getPreferredLanguage().equals("english")){
-           sb.append(ds.getMessageByName(messageName).getEnglishMessage());
-        }
+           sb.append(messageText);
+
         if(ds.getStudentById(studentId).getParent().getPreferredLanguage().equals("spanish")){
-            sb.append(ds.getMessageByName(messageName).getSpanishMessage());
+                sb.append("\n\n");
+                sb.append(ts.translationMessage("The following is an automatic translation of the above message","es","en"));
+                sb.append("\n");
+                sb.append(ts.translationMessage(messageText,"es","en"));
         }
         sb.append("\n\n");
         sb.append("Respectfully, \n");
@@ -129,3 +135,4 @@ public class MessageService {
 
     }
 }
+
---
+++ src/main/java/com/forestnewark/service/TranslationService.java
@@ -0,0 +1,67 @@
+package com.forestnewark.service;
+
+import com.forestnewark.bean.MessageTranslation;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.text.BreakIterator;
+import java.util.Locale;
+
+/**
+ * Created by forestnewark on 5/5/17.
+ */
+@Service
+public class TranslationService {
+
+
+    private String translate(String message, String toLang, String fromLang){
+
+        String requestUrl = "http://www.transltr.org/api/translate";
+
+        MessageTranslation messageToTranslate = new MessageTranslation(message,fromLang,toLang,"");
+
+        RestTemplate restTemplate = new RestTemplate();
+
+       return restTemplate.postForObject(requestUrl,messageToTranslate,MessageTranslation.class).getTranslationText();
+
+    }
+
+    public String translationMessage(String message,String toLang, String fromLang){
+
+        StringBuilder sb = new StringBuilder();
+
+        BreakIterator iterator = BreakIterator.getSentenceInstance(Locale.US);
+
+        String source = message;
+
+        iterator.setText(source);
+
+        int start = iterator.first();
+
+        for (int end = iterator.next();
+             end != BreakIterator.DONE;
+             start = end, end = iterator.next()) {
+
+            sb.append(this.translate(source.substring(start,end),toLang,fromLang));
+
+            String test = source.substring(start,end);
+            if(test.substring(test.length()-1,test.length()).equals(" ")){
+//                System.out.println(test.substring(test.length()-2,test.length()-1));
+                sb.append(test.substring(test.length()-2,test.length()-1));
+            }
+            if(!test.substring(test.length()-1,test.length()).equals(" ")) {
+//                System.out.println(test.substring(test.length()-1,test.length()));
+                sb.append(test.substring(test.length()-1,test.length()));
+            }
+
+        }
+
+
+        return sb.toString();
+
+    }
+
+
+
+
+}
