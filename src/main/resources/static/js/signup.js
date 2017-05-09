c0c03d8a35a7ea750bf62e3a3e74406e21d1fd91
LyndseyR <lyndseyroka@Lyndseys-MacBook-Pro.local>
2017-05-09 13:53:06 -0400

    mobile responsive added styles to teacher message page

--- src/main/resources/static/js/signup.js
+++ src/main/resources/static/js/signup.js
@@ -1,18 +1,9 @@
 // Parent Page Drop Down for second point of contact and students
+$(document).ready(function(){
   $('.addContact').click(function(){
     $(this).next('.contactInfo').slideToggle();
   });
-
-  // $('input').focus(function() {
-  //     $(this).next().css('color','#00b074');
-  //     $(this).next().css('transform','translateY(-27px)');
-  // }).blur(function() {
-  //     if($(this).val() == '') {
-  //       $(this).next().css('color','#333');
-  //       $(this).next().css('transform','translateY(-10px)');
-  //     }
-  // });
-
+});
 
   $('input').focus(function() {
       $(this).css({'border': '1px solid #00b074'});
@@ -30,7 +21,17 @@
       };
   });
 
-  // mobile menu
-  $('.hamburger').click(function(){
-    $('.hamburger-menu').slideToggle();
+
+// mobile menu
+$(document).ready(function(){
+  $('.mobile-toggle').click(function(){
+    $('.menu').slideToggle();
   });
+});
+
+// Teacher Sign in Page Profile Image
+$(document).ready(function(){
+  $('.profile').click(function(){
+    $('#updateProfile').slideToggle();
+  });
+});
