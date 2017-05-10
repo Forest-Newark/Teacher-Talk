08a16d1d8b4e66a64d8388cc5b36461d30d4a166
Forest Newark <forest.newark@gmail.com>
2017-05-09 20:17:00 -0400

    start to fix input fields on teacher html

--- src/main/resources/static/js/signup.js
+++ src/main/resources/static/js/signup.js
@@ -1,20 +1,35 @@
 // Parent Page Drop Down for second point of contact and students
 $(document).ready(function(){
+
   $('.addContact').click(function(){
     $(this).next('.contactInfo').slideToggle();
   });
 });
 
-// Input Form
 
 $(document).ready(function(){
-  if($('input').val()!==''){
-    $(this).css({'border': '1px solid #00b074'});
-    $(this).next().css({
-      'color':'#00b074',
-      'transform': 'translateY(-27px)',
-    });
-  }
+    var inputSelector = $('.input-wrapper');
+    var firstNameValue= inputSelector.find("input[name='registerFirstName']").val();
+    var lastNameValue= inputSelector.find("input[name='registerLastName']").val();
+
+    //firstName
+    if(firstNameValue.length > 0){
+        $('#registerFirstName').css({'border': '1px solid #00b074'});
+        $('#firstNameLabel').css({
+            'color':'#00b074',
+            'transform': 'translateY(-27px)'
+        });
+    }
+
+    if(firstNameValue.length >0){
+        $('#registerLastName').css({'border': '1px solid #00b074'});
+        $('#lastNameLabel').css({
+            'color':'#00b074',
+            'transform': 'translateY(-27px)'
+        });
+    }
+
+
 });
 
   $('input').focus(function() {
@@ -38,12 +53,15 @@ $(document).ready(function(){
 $(document).ready(function(){
   $('.mobile-toggle').click(function(){
     $('.menu').slideToggle();
+
   });
 });
 
 // Teacher Sign in Page Profile Image
 $(document).ready(function(){
   $('.profile').click(function(){
+
+      // $('#firstNameLabel').css( {'color':'#00b074', 'transform': 'translateY(-27px)'});
     $('#updateProfile').slideToggle();
   });
 });
