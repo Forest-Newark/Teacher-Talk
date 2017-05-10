37bc8a271c1efe8bde7946cc711c63612a85af95
LyndseyR <lyndseyroka@Lyndseys-MacBook-Pro.local>
2017-05-10 15:36:19 -0400

    Header has slide toggles for adding student, template and profile, Start tour has red styling

--- src/main/resources/static/js/menu.js
+++ src/main/resources/static/js/menu.js
@@ -29,12 +29,30 @@ $(document).ready(function(){
 $(document).ready(function(){
   $('.student').click(function(){
 
-    if(!$('#addStudent').is(':visible'))
+    if(!$('#addStudent').is(':visible')){
       $('.student').css('background','#fff');
+      $('.student a').css({'-webkit-box-shadow' : 'none', '-moz-box-shadow' : 'none', 'box-shadow' : 'none'});
+    }
 
     $('#addStudent').slideToggle(function(){
-      if(!$('#addStudent').is(':visible'))
-      $('.student').css('background','#faf0a1');
+      if(!$('#addStudent').is(':visible')){
+        $('.student').css('background','#faf0a1');
+        $('a').css({'-webkit-box-shadow' : '0 2px 3px rgba(0, 0, 0, 0.1)', '-moz-box-shadow' : '0 2px 3px rgba(0, 0, 0, 0.1)', 'box-shadow' : '0 2px 3px rgba(0, 0, 0, 0.1)'});
+      } else {
+        $('.student a').css({'-webkit-box-shadow' : 'none', '-moz-box-shadow' : 'none', 'box-shadow' : 'none'});
+      }
+    });
+  });
+});
+
+// Add Template
+$(document).ready(function(){
+  $('.template').click(function(){
+    if(!$('#addTemplate').is(':visible'))
+      $('.template').css('background','#fff')
+    $('#addTemplate').slideToggle(function(){
+      if(!$('#addTemplate').is(':visible'))
+        $('.template').css('background','#faf0a1')
     });
   });
 });
