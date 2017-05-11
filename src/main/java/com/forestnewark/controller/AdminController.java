753ccb9eafe51ba286b84914ce85a3e76be9bfdc
Forest Newark <forest.newark@gmail.com>
2017-05-11 11:33:05 -0400

    added other tables to adminpage

--- src/main/java/com/forestnewark/controller/AdminController.java
+++ src/main/java/com/forestnewark/controller/AdminController.java
@@ -1,6 +1,9 @@
 package com.forestnewark.controller;
 
 import com.forestnewark.repository.MessageRepository;
+import com.forestnewark.repository.ParentRepository;
+import com.forestnewark.repository.StudentRepository;
+import com.forestnewark.repository.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -16,6 +19,15 @@ public class AdminController {
     MessageRepository messageRepository;
 
     @Autowired
+    StudentRepository studentRepository;
+
+    @Autowired
+    ParentRepository parentRepository;
+
+    @Autowired
+    TeacherRepository teacherRepository;
+
+    @Autowired
     public AdminController(MessageRepository messageRepository) {
         this.messageRepository = messageRepository;
     }
@@ -24,7 +36,9 @@ public class AdminController {
     @RequestMapping("/adminpage")
     public String adminPage(ModelMap model) {
         model.addAttribute("messages",messageRepository.findAll());
-
+        model.addAttribute("students",studentRepository.findAll());
+        model.addAttribute("parents",parentRepository.findAll());
+        model.addAttribute("teachers",teacherRepository.findAll());
         return "adminpage";
 
     }
@@ -33,6 +47,25 @@ public class AdminController {
     public RedirectView deleteMessage(Integer messageId){
 
         messageRepository.delete(messageId);
+        return new RedirectView("/adminpage");
+    }
+
+    @RequestMapping("/deleteStudent")
+    public RedirectView deleteStudent(Integer studentId){
+
+        studentRepository.delete(studentId);
+        return new RedirectView("/adminpage");
+    }
+
+    @RequestMapping("/deleteParent")
+    public RedirectView deleteParent(Integer parentId){
+        parentRepository.delete(parentId);
+        return new RedirectView("/adminpage");
+    }
+
+    @RequestMapping("/deleteTeacher")
+    public RedirectView deleteTeacher(Integer teacherId){
+        teacherRepository.delete(teacherId);
         return new RedirectView("/adminpage");
     }
 }
