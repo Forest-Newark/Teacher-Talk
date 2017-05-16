63412fb24a1558a42753de8c95ceb31292a4da42
LyndseyR <lyndseyroka@Lyndseys-MacBook-Pro.local>
2017-05-16 12:37:21 -0400

    debugged hover states on menu in teacher page

--- src/main/resources/static/js/menu.js
+++ src/main/resources/static/js/menu.js
@@ -13,14 +13,33 @@ $(document).ready(function(){
 
 // Menu Drop in Each Section
 $(document).ready(function(){
+
+  $(".menu-section").mouseover(function(){
+        $(this).css({
+          background: '#fff',
+          transition: 'background .5s'
+        });
+  });
+  $(".menu-section").mouseout(function(){
+        $(this).css({
+          background: 'transparent',
+          transition: 'background .5s'
+        });
+  });
+
   $('.menu-section').click(function(){
+
+    $('.menu-section a:focus').css('background', '#fff');
 
     $(this).find('i').toggleClass('fa-angle-down fa-angle-up');
 
       $(this).next().slideToggle(function(){
         if($(this).is(':hidden')){
-          $('.menu-section a:focus').css('background', 'transparent')
+          $('.menu-section a:focus').css('background', 'transparent');
         }
+        // else {
+        //   $('.menu-section a:focus').css('background', '#fff');
+        // }
       });
   });
 });
