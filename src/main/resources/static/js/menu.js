e591c1592efbe3eb19bd601208417e595059c756
LyndseyR <lyndseyroka@Lyndseys-MacBook-Pro.local>
2017-05-16 11:27:46 -0400

    refactored header on teacher page, drop shadow on a links now working

--- src/main/resources/static/js/menu.js
+++ src/main/resources/static/js/menu.js
@@ -11,55 +11,16 @@ $(document).ready(function(){
 // Header Slide Toggles
 /////////////////////////
 
-// Profile
+// Menu Drop in Each Section
 $(document).ready(function(){
-  $('.profile').click(function(){
+  $('.menu-section').click(function(){
 
     $(this).find('i').toggleClass('fa-angle-down fa-angle-up');
 
-    if(!$('#updateProfile').is(':visible'))
-      $('.profile').css('background','#fff');
-
-    $('#updateProfile').slideToggle(function(){
-      if(!$('#updateProfile').is(':visible'))
-      $('.profile').css('background','#faf0a1');
-    });
-  });
-});
-
-// Add Student
-$(document).ready(function(){
-  $('.student').click(function(){
-
-    $(this).find('i').toggleClass('fa-angle-down fa-angle-up');
-
-    if(!$('#addStudent').is(':visible')){
-      $('.student').css('background','#fff');
-      $('.student a').css({'-webkit-box-shadow' : 'none', '-moz-box-shadow' : 'none', 'box-shadow' : 'none'});
-    }
-
-    $('#addStudent').slideToggle(function(){
-      if(!$('#addStudent').is(':visible')){
-        $('.student').css('background','#faf0a1');
-        $('a').css({'-webkit-box-shadow' : '0 2px 3px rgba(0, 0, 0, 0.1)', '-moz-box-shadow' : '0 2px 3px rgba(0, 0, 0, 0.1)', 'box-shadow' : '0 2px 3px rgba(0, 0, 0, 0.1)'});
-      } else {
-        $('.student a').css({'-webkit-box-shadow' : 'none', '-moz-box-shadow' : 'none', 'box-shadow' : 'none'});
-      }
-    });
-  });
-});
-
-// Add Template
-$(document).ready(function(){
-  $('.template').click(function(){
-
-    $(this).find('i').toggleClass('fa-angle-down fa-angle-up');
-    
-    if(!$('#addTemplate').is(':visible'))
-      $('.template').css('background','#fff')
-    $('#addTemplate').slideToggle(function(){
-      if(!$('#addTemplate').is(':visible'))
-        $('.template').css('background','#faf0a1')
-    });
+      $(this).next().slideToggle(function(){
+        if($(this).is(':hidden')){
+          $('.menu-section a:focus').css('background', 'transparent')
+        }
+      });
   });
 });
