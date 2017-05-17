aa728dfd46dc49d722419f615885a21d27a6c9dd
RoganRhysShane <chrisoliver08@live.com>
2017-05-17 10:50:21 -0400

    database info parent

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,6 +4,8 @@
 INSERT INTO teacher(id, email, first_name, last_name, password, phone_number, subject_area) VALUES (1,'forest.newark@gmail.com','forest','newark','admin','9194528196','music');
 
 --PARENT INSERT
+INSERT INTO parent(id, password, phone_number, preferred_contact_method, preferred_language, primary_email, primary_first_name, primary_last_name, secondary_email, secondary_first_name, secondary_last_name) VALUES (1,'3222342333','email','english','simmons@gmail.com','bob','simmons','joe@gmail.com', 'jess', 'simmons');
+INSERT INTO parent(id, password, phone_number, preferred_contact_method, preferred_language, primary_email, primary_first_name, primary_last_name, secondary_email, secondary_first_name, secondary_last_name) VALUES (2,'2323223333','email','english','hudson@gmail.com','greg','hudson','hudson@gmail.com', 'kat', 'hudson');
 
 
 --STUDENT INSERT
